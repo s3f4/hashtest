aeee399a3b588869cbf11e9df5cced2f31631309
Sefa Şahin <sefa@dehaa.com>
2024-02-09 15:39:11 +0300

    new version 35

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
-const version = 40;
+const version = 41;
 app.get("/", (req, res) => {
   res.send(`Hello World! v${version}`);
 });
